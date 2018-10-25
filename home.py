53b9b9ecec1cf0347644bcd365e1c9ca2efc3f94
Amine M. Boulouma <amine@boulouma.com>
2018-10-25 21:15:02 +0200

    Enhancements

---
+++ home.py
@@ -0,0 +1,7 @@
+print(" ".join([k for k in ['Opinionytics',
+
+                            '\nA', 'simple', 'way', 'to',
+                            'view', 'all', 'the', 'informations',
+                            'about', 'any', 'text', 'out', 'there.']
+                ])
+      )
